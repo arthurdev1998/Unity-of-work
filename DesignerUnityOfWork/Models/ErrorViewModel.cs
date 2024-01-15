281eca88ebe431a23886ee9ba60c83033a30e58e
Arthur Gomes <arthur.silva@neovero.com>
2024-01-15 19:18:26 -0300

    Initial Correction

---
+++ DesignerUnityOfWork/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DesignerUnityOfWork.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
